8f670f7a34ca155fdfbee0d847097537d8549051
Dinnysbbbakery <dinnyren12@gmail.com>
2024-05-02 16:17:15 +1000

    m

--- Week 6/script.js
+++ Week 6/script.js
@@ -40,26 +40,40 @@
 //     // // console.log(myDiv);
 //     // myDiv.classList.toggle("coral-box");
 // }
- const myButton = document.querySelector("#my-button");
- console.log(myButton);
-myButton.addEventListener("click", chooseTopic);
-function chooseTopic()
-{
-    const myProjects=document.querySelectorAll("p");
-    myProjects.forEach(changeColor);
-    function changeColor(item)
-    {
-        if(item.dataset.topic==="web")
-        {
-            item.classList.add("purple-box");
-        }
-        else if (item.dataset.topic==="sound")
-        {
-            item.classList.add("coral-box");
-        }
-        else if (item.dataset.topic==="games")
-        {
-            item.classList.add("lime-box");
-        }
-    }
+//  const myButton = document.querySelector("#my-button");
+//  console.log(myButton);
+// myButton.addEventListener("click", chooseTopic);
+// function chooseTopic()
+// {
+//     const myProjects=document.querySelectorAll("p");
+//     myProjects.forEach(changeColor);
+//     function changeColor(item)
+//     {
+//         if(item.dataset.topic==="web")
+//         {
+//             item.classList.add("purple-box");
+//         }
+//         else if (item.dataset.topic==="sound")
+//         {
+//             item.classList.add("coral-box");
+//         }
+//         else if (item.dataset.topic==="games")
+//         {
+//             item.classList.add("lime-box");
+//         }
+//     }
+// }
+
+const myHeading=document.querySelector("h1");
+myHeading.textContent="new Heading";
+myHeading.innerHTML=`new<span class="coral-box> heading </span>`;
+
+const myImg=document.querySelector("cat-image");
+myImg.addEventListener("mouseover", makeItRound);
+myImg.addEventListener("mouseout", makeItSquare);
+function makeItRound(){
+    myImg.classList.add("round");
+}
+function makeItSquare(){
+    myImg.classList.remove("round");
 }

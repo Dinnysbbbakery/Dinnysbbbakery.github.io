2f94523894d74519dc79159e54cc479db7e94cf6
Dinnysbbbakery <162535730+Dinnysbbbakery@users.noreply.github.com>
2024-06-03 21:42:34 +1000

    c

--- asiggment3/script.js
+++ asiggment3/script.js
@@ -1,32 +1,52 @@
-let draggedElement=null;
+const close =document.querySelector(".ready");
+const box =document.querySelector(".rules").style.display="none";
 
-const one=document.querySelector(".item2");
-const two=document.querySelector(".item5");
-const three=document.querySelector(".item6");
-const centre1=document.querySelector(".position1");
-const centre2=document.querySelector(".position2");
-const centre3=document.querySelector(".position3");
 
-one.addEventListener("dragstart", startDrag);
+
+close.addEventListener('click', function(){
+    box.style.display="none";
+});
+
+
+
+
+
+// let draggedElement=null;
+
+// const one=document.querySelector(".item2");
+// const two=document.querySelector(".item5");
+// const three=document.querySelector(".item6");
+// const centre1=document.querySelector(".position1");
+// const centre2=document.querySelector(".position2");
+// const centre3=document.querySelector(".position3");
+
+// one.addEventListener("dragstart", startDrag);
 // two.addEventListener("dragstart", startDrag);
 
-function startDrag(){
-    draggedElement=one;
-}
+// function startDrag(){
+//     draggedElement=one;
+// }
 
-centre1.addEventListener("dragover", endDrag);
+// centre1.addEventListener("dragover", endDrag);
 
-function endDrag(event){
-    event.preventDafault();
-}
+// function endDrag(event){
+//     event.preventDafault();
+// }
 
-centre1.addEventListener("drop", handleDrop);
+// centre1.addEventListener("drop", handleDrop);
 
-function handleDrop(){
-    if (draggedElement){
-        const 
-    }
-}
+// function handleDrop(){
+//     if (draggedElement){
+//         const 
+//     }
+// }
+
+// function endDrag(){
+//     centre1.prepend(one);
+// }
+
+
+
 
 
 
